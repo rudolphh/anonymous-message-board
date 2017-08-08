d5a399d4c2bb4cbacac8729f0be030a2db00b4b6
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-08-07 22:40:54 -0700

    Stub http meethods for api routes

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,33 @@
 var expect = require('chai').expect;
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+
+  app.route('/api/threads/:board')
+    .get(function(req, res){
+
+    })
+    .post(function(req, res){
+
+    })
+    .put(function(req, res){
+
+    })
+    .delete(function(req, res){
+
+    });
+
+  app.route('/api/replies/:board')
+    .get(function(req, res){
+
+    })
+    .post(function(req, res){
+
+    })
+    .put(function(req, res){
+
+    })
+    .delete(function(req, res){
+
+    });
 
 };
